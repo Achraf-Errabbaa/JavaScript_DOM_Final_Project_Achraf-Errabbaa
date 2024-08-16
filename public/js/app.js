a118b76ab83f091ceb272446b28b0d403cb6cd6d
mortyrex34 <achraferrabbaa1@gmail.com>
2024-08-16 12:55:41 +0100

    imonials and chefs

--- public/js/app.js
+++ public/js/app.js
@@ -39,3 +39,50 @@ lunch.addEventListener('click',() => {
 dinner.addEventListener('click',() => {
     namme.textContent = 'dinner'
 })
+
+//* critics section
+const slides = document.querySelectorAll('.slides .slide')
+let slideIndex = 0
+
+document.addEventListener("DOMContentLoaded", initializeSlider);
+
+function initializeSlider() {
+    if (slides.length > 0) {
+        slides[slideIndex].classList.add('displaySlide')
+        const circles = document.querySelectorAll('.circle')
+        circles.forEach((circle, index) => {
+            circle.addEventListener('click', () => {
+                showSlide(index)
+            })
+        })
+        setInterval(() => {
+            showSlide(slideIndex + 1)
+        }, 5000)
+    }
+}
+
+function showSlide(Index) {
+    if (Index >= slides.length) {
+        slideIndex = 0;
+    } else if (Index < 0) {
+        slideIndex = slides.length - 1;
+    } else {
+        slideIndex = Index;
+    }
+    slides.forEach(slide => {
+        slide.classList.remove('displaySlide');
+    })
+    slides[slideIndex].classList.add('displaySlide')
+    updateCircleNav(slideIndex)
+}
+
+function updateCircleNav(slideIndex) {
+    const circles = document.querySelectorAll('.circle')
+    circles.forEach((circle, index) => {
+        if (index === slideIndex) {
+            circle.classList.add('active')
+        } else {
+            circle.classList.remove('active')
+        }
+    })
+}
